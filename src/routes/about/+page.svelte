00ca3c0974f9b5bdea6557a813831205d88402ab
Daniils Petrovs <daniils@platogo.com>
2023-06-03 23:32:56 +0200

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="text-2xl text-center mx-4 space-y-8">
+	<p>HoloQuest companion app</p>
+
+	<p>Developed by <a href="https://watsonindustries.live" class="link link-primary">Watson Industries</a></p>
+
+    <p>Contact <a href="https://twitter.com/DaniruKun" class="link link-secondary">@danirukun</a> about questions</p>
+</div>
