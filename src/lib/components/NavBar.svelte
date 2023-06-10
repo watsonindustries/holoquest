fc69675852914ed11c9c02110c605108b0e44174
Daniils Petrovs <daniils@platogo.com>
2023-06-10 17:51:21 +0200

    Improe navbar styling

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,32 +1,16 @@
 <script lang="ts">
 	import { Icon } from '@steeze-ui/svelte-icon';
-
-	//import as many icons from a Icon Pack
-	import { Home, QrCode, QuestionMarkCircle, UserCircle } from '@steeze-ui/heroicons';
+	import { Bars2, Home, QrCode, QuestionMarkCircle, UserCircle } from '@steeze-ui/heroicons';
 	import ConnectionIndicator from './ConnectionIndicator.svelte';
-
 </script>
 
 <div class="navbar bg-base-100">
 	<div class="navbar-start">
 		<div class="dropdown">
-			<label tabindex="0" class="btn-ghost btn-circle btn">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h16M4 18h7"
-					/></svg
-				>
-			</label>
+			<button class="btn-ghost btn-circle btn">
+				<Icon src={Bars2} size=24 style="solid"></Icon>
+			</button>
 			<ul
-				tabindex="0"
 				class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
 			>
 				<li><a href="/"><Icon src={Home} theme="solid" class="color-gray-900 w-4" />Home</a></li>
