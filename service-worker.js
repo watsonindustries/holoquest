9ec9c60196b133b3866fc061913f625985c66e3e
DaniruKun <DaniruKun@users.noreply.github.com>
2023-06-14 21:35:40 +0000

    deploy: bc659945fadf8f880ffbc0711b7e7128f4f0a05d

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,28 @@
-const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), h = [
-  s + "/_app/immutable/entry/app.490e51b6.js",
-  s + "/_app/immutable/assets/0.23b00dc7.css",
-  s + "/_app/immutable/nodes/0.03201873.js",
-  s + "/_app/immutable/nodes/1.9a92f9a3.js",
-  s + "/_app/immutable/nodes/2.397b0553.js",
-  s + "/_app/immutable/nodes/3.ea3c153d.js",
-  s + "/_app/immutable/nodes/4.1e70d41b.js",
-  s + "/_app/immutable/nodes/5.fe7ec4f7.js",
-  s + "/_app/immutable/chunks/const.20bb8ed5.js",
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), u = [
+  s + "/_app/immutable/entry/app.4dcd43cf.js",
+  s + "/_app/immutable/assets/0.d03c7c73.css",
+  s + "/_app/immutable/nodes/0.bc785264.js",
+  s + "/_app/immutable/nodes/1.94b66830.js",
+  s + "/_app/immutable/nodes/2.6db1009b.js",
+  s + "/_app/immutable/nodes/3.6d901ef0.js",
+  s + "/_app/immutable/nodes/4.2427faf7.js",
+  s + "/_app/immutable/nodes/5.7f59020c.js",
+  s + "/_app/immutable/nodes/6.fa2d3be6.js",
+  s + "/_app/immutable/chunks/client.2f60997e.js",
+  s + "/_app/immutable/chunks/const.e86bc028.js",
   s + "/_app/immutable/chunks/crypto.6f6cc81d.js",
-  s + "/_app/immutable/chunks/index.109b436a.js",
-  s + "/_app/immutable/chunks/index.74714e33.js",
+  s + "/_app/immutable/chunks/custom.b03ad683.js",
+  s + "/_app/immutable/chunks/index.452c6ad5.js",
+  s + "/_app/immutable/chunks/index.a116848c.js",
+  s + "/_app/immutable/chunks/index.f2e2f952.js",
+  s + "/_app/immutable/chunks/index.f45b4d2e.js",
   s + "/_app/immutable/chunks/preload-helper.41c905a7.js",
   s + "/_app/immutable/chunks/qr-scanner.min.4880ecae.js",
-  s + "/_app/immutable/chunks/singletons.88e9f2e3.js",
-  s + "/_app/immutable/entry/start.6cec3512.js",
+  s + "/_app/immutable/chunks/singletons.dff79676.js",
+  s + "/_app/immutable/chunks/store.76160239.js",
+  s + "/_app/immutable/entry/start.fa65c0fe.js",
   s + "/_app/immutable/chunks/qr-scanner-worker.min.5f44a019.js"
-], u = [
+], h = [
   s + "/favicon.png",
   s + "/fonts/Geologica.ttf",
   s + "/icons/android-chrome-192x192.png",
@@ -29,46 +35,46 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), h
   s + "/logo_512.png",
   s + "/manifest.json",
   s + "/robots.txt"
-], i = "1685897045034", a = self, p = `cache${i}`, r = `offline${i}`, d = ["/", "/result", "/scanner", "/about"], f = [
+], i = "1686778530719", n = self, p = `cache${i}`, m = `offline${i}`, d = ["/", "/result", "/scanner", "/about"], f = [
   "https://4.bp.blogspot.com/-ZMzPgyP64Zg/WFuJyby0eDI/AAAAAAABAmk/I_UtFowN0ecaQiqgYGpTsKGTqFzMLESdwCLcB/s800/nenga_hanko_kingashinnen_maru.png"
 ], b = (e) => e.map((t) => self.location.origin + t), _ = b([
-  ...u.filter((e) => !/\/icons\/(apple.*?|original.png)/.test(e)),
-  ...h,
+  ...h.filter((e) => !/\/icons\/(apple.*?|original.png)/.test(e)),
+  ...u,
   ...d
-]), l = [..._, ...f], g = new Set(l);
-a.addEventListener("install", (e) => {
+]), r = [..._, ...f], g = new Set(r);
+n.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(p).then((t) => t.addAll(l)).then(() => {
-      a.skipWaiting();
+    caches.open(p).then((t) => t.addAll(r)).then(() => {
+      n.skipWaiting();
     })
   );
 });
-a.addEventListener("activate", (e) => {
+n.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(async (t) => {
-      for (const n of t)
-        n !== p && n !== r && await caches.delete(n);
-      a.clients.claim();
+      for (const a of t)
+        a !== p && a !== m && await caches.delete(a);
+      n.clients.claim();
     })
   );
 });
-async function A(e) {
-  const t = await caches.open(r);
+async function j(e) {
+  const t = await caches.open(m);
   try {
-    const n = await fetch(e);
-    return t.put(e, n.clone()), n;
-  } catch (n) {
+    const a = await fetch(e);
+    return t.put(e, a.clone()), a;
+  } catch (a) {
     const c = await t.match(e);
     if (c)
       return c;
-    throw n;
+    throw a;
   }
 }
-a.addEventListener("fetch", (e) => {
+n.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const t = new URL(e.request.url), n = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, o = g.has(t.href), m = e.request.cache === "only-if-cached" && !o;
-  n && !c && !m && e.respondWith(
-    (async () => o && await caches.match(e.request) || A(e.request))()
+  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, o = g.has(t.href), l = e.request.cache === "only-if-cached" && !o;
+  a && !c && !l && e.respondWith(
+    (async () => o && await caches.match(e.request) || j(e.request))()
   );
 });
