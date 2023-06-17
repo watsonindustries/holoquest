0c492d0f3c0b01b483e259932c3f4e193ae1ea35
Daniils Petrovs <daniils@platogo.com>
2023-06-17 11:00:19 +0200

    Fix service worker
    Closes Fix Workbox of the service worker #2

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import { defineConfig } from 'vitest/config';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
-		https: true
+		https: false
+	},
+	define: {
+		'process.env.NODE_ENV': process.env.NODE_ENV === 'production'
+			? '"production"'
+			: '"development"'
 	}
 });
