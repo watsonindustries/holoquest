877ebf06fc9b3a6ecfe1e504bf54662f8363cbb2
Daniils Petrovs <daniils@platogo.com>
2023-06-17 23:18:16 +0200

    Hide drawer on navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -2,15 +2,22 @@
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Home, QrCode, QuestionMarkCircle, Cog } from '@steeze-ui/heroicons';
 	import ConnectionIndicator from './ConnectionIndicator.svelte';
+	import { onMount } from 'svelte';
+
+	let drawerToggle: HTMLInputElement;
+
+	onMount(() => {
+		drawerToggle = document.querySelector('.drawer-toggle') as HTMLInputElement;
+	});
 </script>
 
 <div class="drawer">
-	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<input id="drawer-nav" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col">
 		<!-- Navbar -->
 		<div class="navbar mb-4 flex w-full justify-between bg-base-300">
 			<div class="flex-none lg:hidden">
-				<label for="my-drawer-3" class="btn-ghost btn-square btn">
+				<label for="drawer-nav" class="btn-ghost btn-square btn">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -40,24 +47,40 @@
 		<slot />
 	</div>
 	<div class="drawer-side">
-		<label for="my-drawer-3" class="drawer-overlay" />
+		<label for="drawer-nav" class="drawer-overlay" />
 		<ul class="menu h-full w-60 bg-base-200 p-4">
 			<!-- Sidebar content here -->
 			<li>
-				<a href="/"><Icon src={Home} theme="solid" class="color-gray-900" size="20" />Home</a>
+				<a
+					href="/"
+					on:click={() => {
+						drawerToggle.checked = false;
+					}}><Icon src={Home} theme="solid" class="color-gray-900" size="20" />Home</a>
 			</li>
 			<li>
-				<a href="/scanner">
+				<a
+					href="/scanner"
+					on:click={() => {
+						drawerToggle.checked = false;
+					}}>
 					<Icon src={QrCode} theme="solid" class="color-gray-900" size="20" />Scanner
 				</a>
 			</li>
 			<li>
-				<a href="/settings">
+				<a
+					href="/settings"
+					on:click={() => {
+						drawerToggle.checked = false;
+					}}>
 					<Icon src={Cog} theme="solid" class="color-gray-900" size="20" />Settings
 				</a>
 			</li>
 			<li>
-				<a href="/about">
+				<a
+					href="/about"
+					on:click={() => {
+						drawerToggle.checked = false;
+					}}>
 					<Icon src={QuestionMarkCircle} theme="solid" class="color-gray-900" size="20" />About
 				</a>
 			</li>
