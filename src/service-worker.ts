703467fd3cef05efde81f9aa43b371f122d94323
Daniils Petrovs <daniils@platogo.com>
2023-06-22 23:27:20 +0200

    Update service worker to skip waiting

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,6 +3,8 @@
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
 
+const sw = self as unknown as ServiceWorkerGlobalScope;
+
 import { build, files, prerendered, version } from '$service-worker';
 import { precacheAndRoute } from 'workbox-precaching';
 
@@ -12,3 +14,15 @@ const precache_list = [...build, ...files, ...prerendered].map((s) => ({
 }));
 
 precacheAndRoute(precache_list);
+
+sw.addEventListener("install", event => {
+	// forces a service worker to activate immediately
+	console.log("installing service worker version: ", version);
+	sw.skipWaiting();
+});
+
+sw.addEventListener("activate", event => {
+	// when this SW becomes activated, we claim all the opened clients
+	// they can be standalone PWA windows or browser tabs
+	event.waitUntil(sw.clients.claim());
+});
