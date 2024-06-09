11ea255372527efce8f5a9ecd4f310f00a8f2bff
Daniils Petrovs <daniils@platogo.com>
2024-06-09 20:37:25 +0200

    Add vite plugin url copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import ServerUrlCopy from 'vite-plugin-url-copy';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), ServerUrlCopy({ qrcode: { disabled: false } })],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
+		host: true,
 		https: false
+	},
+	preview: {
+		host: true
 	},
 	define: {
 		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"'
